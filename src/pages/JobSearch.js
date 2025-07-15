94243551bd3f7ff058982c9e842d0c4f7499e7e8
Eva Li <eva@evas-air.lan>
2025-07-14 21:02:27 -0400

    added Job Search to nav

---
+++ src/pages/JobSearch.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar';
+export default function Calendar() {
+    return (
+      <div className="jobsearch">
+        <Navbar />
+        <h1>Search for Jobs</h1>
+        
+      </div>
+    );
+  }
