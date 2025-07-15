16484f936083c4a3716d410ab9d6a36af3bd7fd0
luciano093 <luparedes100@gmail.com>
2025-07-14 21:32:48 -0400

    Add mock api response to job search page

--- src/pages/JobSearch.js
+++ src/pages/JobSearch.js
@@ -1,5 +1,12 @@
 import Navbar from '../components/Navbar';
 export default function Calendar() {
+  console.log(process.env.REACT_APP_APP_KEY);
+    let api = `http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&results_per_page=20&what=javascript%20developer&content-type=application/json`;
+
+    fetch(api).then(async (response) => {
+      console.log(await response.json());
+    });
+
     return (
       <div className="jobsearch">
         <Navbar />
