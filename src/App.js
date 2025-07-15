511360269f27a29f5dae8ad859e6cd27d14c7ef1
Eva Li <evali767@gmail.com>
2025-07-15 12:39:48 -0400

    Capitalized titles in job search page

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
   return (
     <Router>
       <div className="app">
+        <Navbar/>
         {/* {isLoggedIn && <Navbar />} */}
         <Routes>
           {/* <Route path="/" element={
